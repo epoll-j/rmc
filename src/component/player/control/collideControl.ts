3c037ecb90db00ca0df2176c72dab0663b0f0f6d
epoll-j <epoll@foxmail.com>
2023-06-16 18:52:59 +0800

    test: collide check

--- src/component/player/control/collideControl.ts
+++ src/component/player/control/collideControl.ts
@@ -10,28 +10,28 @@ export class CollideControl {
     this.initRayCaster();
   }
 
-  private raycasterDown = new Raycaster();
-  private raycasterUp = new Raycaster();
-  private raycasterFront = new Raycaster();
-  private raycasterBack = new Raycaster();
-  private raycasterRight = new Raycaster();
-  private raycasterLeft = new Raycaster();
-  private matrix = new Matrix4()
+  #raycasterDown = new Raycaster();
+  #raycasterUp = new Raycaster();
+  #raycasterFront = new Raycaster();
+  #raycasterBack = new Raycaster();
+  #raycasterRight = new Raycaster();
+  #raycasterLeft = new Raycaster();
+  #matrix = new Matrix4()
 
   initRayCaster() {
-    this.raycasterUp.ray.direction = new Vector3(0, 1, 0);
-    this.raycasterDown.ray.direction = new Vector3(0, -1, 0);
-    this.raycasterFront.ray.direction = new Vector3(1, 0, 0);
-    this.raycasterBack.ray.direction = new Vector3(-1, 0, 0);
-    this.raycasterLeft.ray.direction = new Vector3(0, 0, -1);
-    this.raycasterRight.ray.direction = new Vector3(0, 0, 1);
+    this.#raycasterUp.ray.direction = new Vector3(0, 1, 0);
+    this.#raycasterDown.ray.direction = new Vector3(0, -1, 0);
+    this.#raycasterFront.ray.direction = new Vector3(1, 0, 0);
+    this.#raycasterBack.ray.direction = new Vector3(-1, 0, 0);
+    this.#raycasterLeft.ray.direction = new Vector3(0, 0, -1);
+    this.#raycasterRight.ray.direction = new Vector3(0, 0, 1);
 
-    this.raycasterUp.far = 1.2;
-    this.raycasterDown.far = 1.8;
-    this.raycasterFront.far = 1;
-    this.raycasterBack.far = 1;
-    this.raycasterLeft.far = 1;
-    this.raycasterRight.far = 1;
+    this.#raycasterUp.far = 1.2;
+    this.#raycasterDown.far = 1.8;
+    this.#raycasterFront.far = 1;
+    this.#raycasterBack.far = 1;
+    this.#raycasterLeft.far = 1;
+    this.#raycasterRight.far = 1;
   }
 
   check(side: CollideSide, position: Vector3, blockMap: Map<string, number>): boolean {
@@ -45,29 +45,51 @@ export class CollideControl {
       new Float32Array(100 * 16),
       16
     )
+    // let x = Math.round(position.x)
+    // let z = Math.round(position.z)
     let index = 0
-    const blockId = blockMap.get(`${floorPosition.x}_${Math.floor(position.y - this.raycasterDown.far)}_${floorPosition.z}`)
-    if (!blockId) {
-      return false
-    }
-    this.matrix.setPosition(Math.floor(position.x), Math.floor(position.y - this.raycasterDown.far), Math.floor(position.z))
-    tempMesh.setMatrixAt(index++, this.matrix)
+    const temp = new Vector3()
     switch(side) {
       case CollideSide.down:
-        this.raycasterDown.ray.origin = position
+        temp.set(floorPosition.x, Math.floor(position.y - this.#raycasterDown.far), floorPosition.z)
+        if (blockMap.get(this.getPositionId(temp))) {
+          console.log(position)
+          this.#matrix.setPosition(temp)
+          tempMesh.setMatrixAt(index++, this.#matrix)
+        }
+        if (blockMap.get(this.getPositionId(floorPosition))) {
+          this.#matrix.setPosition(floorPosition)
+          tempMesh.setMatrixAt(index++, this.#matrix)
+        }
+        break
+    }
+    
+    
+    switch(side) {
+      case CollideSide.down:
+        this.#raycasterDown.ray.origin = new Vector3(position.x, position.y - 1, position.z)
     }
 
     tempMesh.instanceMatrix.needsUpdate = true
 
     switch(side) {
       case CollideSide.down:
-        return this.raycasterDown.intersectObject(tempMesh).length != 0
+        return this.#raycasterDown.intersectObject(tempMesh).length != 0
     }
 
     return false;
   }
 
+
   private getFloorPosition(position: Vector3): Vector3 {
     return new Vector3(Math.floor(position.x), Math.floor(position.y), Math.floor(position.z))
+  }
+
+  private getCeilPosition(position: Vector3): Vector3 {
+    return new Vector3(Math.ceil(position.x), Math.ceil(position.y), Math.ceil(position.z))
+  }
+
+  private getPositionId(position: Vector3) {
+    return `${position.x}_${position.y}_${position.z}`
   }
 }
