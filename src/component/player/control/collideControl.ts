185a348e8b7bb9b535423c37102d154db8c56bfb
epoll-j <epoll@foxmail.com>
2023-06-16 17:52:57 +0800

    perf: collide control field make to private

--- src/component/player/control/collideControl.ts
+++ src/component/player/control/collideControl.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { BoxGeometry, InstancedMesh, Matrix4, MeshBasicMaterial, Raycaster, Vector3, Vector4 } from "three";
+import { BoxGeometry, InstancedMesh, Matrix4, MeshBasicMaterial, Raycaster, Vector3 } from "three";
 
 export enum CollideSide {
   down,
@@ -10,13 +10,13 @@ export class CollideControl {
     this.initRayCaster();
   }
 
-  raycasterDown = new Raycaster();
-  raycasterUp = new Raycaster();
-  raycasterFront = new Raycaster();
-  raycasterBack = new Raycaster();
-  raycasterRight = new Raycaster();
-  raycasterLeft = new Raycaster();
-  matrix = new Matrix4()
+  private raycasterDown = new Raycaster();
+  private raycasterUp = new Raycaster();
+  private raycasterFront = new Raycaster();
+  private raycasterBack = new Raycaster();
+  private raycasterRight = new Raycaster();
+  private raycasterLeft = new Raycaster();
+  private matrix = new Matrix4()
 
   initRayCaster() {
     this.raycasterUp.ray.direction = new Vector3(0, 1, 0);
@@ -34,7 +34,8 @@ export class CollideControl {
     this.raycasterRight.far = 1;
   }
 
-  check(side: CollideSide, position: Vector3, blockMap: Map<string, number>) {
+  check(side: CollideSide, position: Vector3, blockMap: Map<string, number>): boolean {
+    const floorPosition = this.getFloorPosition(position)
     const tempMesh = new InstancedMesh(
       new BoxGeometry(1, 1, 1),
       new MeshBasicMaterial(),
@@ -45,8 +46,7 @@ export class CollideControl {
       16
     )
     let index = 0
-    const blockId = blockMap.get(`${Math.floor(position.x)}_${Math.floor(position.y - this.raycasterDown.far)}_${Math.floor(position.z)}`)
-    // console.log(blockId, position)
+    const blockId = blockMap.get(`${floorPosition.x}_${Math.floor(position.y - this.raycasterDown.far)}_${floorPosition.z}`)
     if (!blockId) {
       return false
     }
@@ -65,5 +65,9 @@ export class CollideControl {
     }
 
     return false;
+  }
+
+  private getFloorPosition(position: Vector3): Vector3 {
+    return new Vector3(Math.floor(position.x), Math.floor(position.y), Math.floor(position.z))
   }
 }
