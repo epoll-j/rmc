2794225178f12206e7ae97369e2035720fc8378d
epoll-j <epoll@foxmail.com>
2023-06-26 18:16:34 +0800

    feat: add collide check

--- src/component/player/control/collideControl.ts
+++ src/component/player/control/collideControl.ts
@@ -3,7 +3,12 @@ import * as THREE from 'three';
 import { BoxGeometry, InstancedMesh, Matrix4, MeshBasicMaterial, Raycaster, Vector3 } from "three";
 
 export enum CollideSide {
+  front,
+  back,
+  left,
+  right,
   down,
+  up
 }
 
 export class CollideControl {
@@ -11,29 +16,38 @@ export class CollideControl {
     this.initRayCaster();
   }
 
-  #raycasterDown = new Raycaster();
-  #raycasterUp = new Raycaster();
-  #raycasterFront = new Raycaster();
-  #raycasterBack = new Raycaster();
-  #raycasterRight = new Raycaster();
-  #raycasterLeft = new Raycaster();
+  #raycasterMap = new Map<CollideSide, Raycaster>()
   #matrix = new Matrix4()
   #noise = Noise.getInstance()
 
   initRayCaster() {
-    this.#raycasterUp.ray.direction = new Vector3(0, 1, 0);
-    this.#raycasterDown.ray.direction = new Vector3(0, -1, 0);
-    this.#raycasterFront.ray.direction = new Vector3(1, 0, 0);
-    this.#raycasterBack.ray.direction = new Vector3(-1, 0, 0);
-    this.#raycasterLeft.ray.direction = new Vector3(0, 0, -1);
-    this.#raycasterRight.ray.direction = new Vector3(0, 0, 1);
+    const raycasterDown = new Raycaster();
+    const raycasterUp = new Raycaster();
+    const raycasterFront = new Raycaster();
+    const raycasterBack = new Raycaster();
+    const raycasterRight = new Raycaster();
+    const raycasterLeft = new Raycaster();
 
-    this.#raycasterUp.far = 1.2;
-    this.#raycasterDown.far = 1.8;
-    this.#raycasterFront.far = 1;
-    this.#raycasterBack.far = 1;
-    this.#raycasterLeft.far = 1;
-    this.#raycasterRight.far = 1;
+    raycasterUp.ray.direction = new Vector3(0, 1, 0);
+    raycasterDown.ray.direction = new Vector3(0, -1, 0);
+    raycasterFront.ray.direction = new Vector3(1, 0, 0);
+    raycasterBack.ray.direction = new Vector3(-1, 0, 0);
+    raycasterLeft.ray.direction = new Vector3(0, 0, -1);
+    raycasterRight.ray.direction = new Vector3(0, 0, 1);
+
+    raycasterUp.far = 1.2;
+    raycasterDown.far = 1.8;
+    raycasterFront.far = 1;
+    raycasterBack.far = 1;
+    raycasterLeft.far = 1;
+    raycasterRight.far = 1;
+
+    this.#raycasterMap.set(CollideSide.front, raycasterFront)
+    this.#raycasterMap.set(CollideSide.back, raycasterBack)
+    this.#raycasterMap.set(CollideSide.left, raycasterLeft)
+    this.#raycasterMap.set(CollideSide.right, raycasterRight)
+    this.#raycasterMap.set(CollideSide.down, raycasterDown)
+    this.#raycasterMap.set(CollideSide.up, raycasterUp)
   }
 
   check(side: CollideSide, position: Vector3): boolean {
@@ -48,28 +62,37 @@ export class CollideControl {
     )
     let x = Math.round(position.x)
     let z = Math.round(position.z)
+    
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const raycaster = this.#raycasterMap.get(side)!
+    raycaster.ray.origin = new Vector3().copy(position)
+
+    switch(side) {
+      case CollideSide.front:
+        x += 1
+        break
+      case CollideSide.back:
+        x -= 1
+        break
+      case CollideSide.left:
+        z -= 1
+        break
+      case CollideSide.right:
+        z += 1
+        break
+    }
+    
     const y =
       Math.floor(
         this.#noise.get(x / this.#noise.gap, z / this.#noise.gap, this.#noise.seed) * this.#noise.amp
       ) + 30
-    let index = 0
     
-    switch(side) {
-      case CollideSide.down:
-        this.#matrix.setPosition(x, y, z)
-        tempMesh.setMatrixAt(index++, this.#matrix)
-        this.#raycasterDown.ray.origin = new Vector3().copy(position)
-        break
-    }
+    this.#matrix.setPosition(x, y, z)
+    tempMesh.setMatrixAt(0, this.#matrix)
 
     tempMesh.instanceMatrix.needsUpdate = true
-
-    switch(side) {
-      case CollideSide.down:
-        return this.#raycasterDown.intersectObject(tempMesh).length != 0
-    }
-
-    return false;
+    
+    return raycaster.intersectObject(tempMesh).length != 0
   }
 
 
